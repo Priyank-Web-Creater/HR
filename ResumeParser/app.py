17e1ffcfe5df14ccdd1fe4ef26397b3e2ac1a9d1
Priyank-Web-Creater <priyankp@wisdomsquare.net>
2025-06-03 14:57:19 -0400

    Update app.py

--- ResumeParser/app.py
+++ ResumeParser/app.py
@@ -11,7 +11,6 @@ import logging
 import warnings
 import re
 import time
-import textract
 
 # Suppress warnings
 warnings.filterwarnings("ignore", message="CropBox missing from /Page, defaulting to MediaBox")
